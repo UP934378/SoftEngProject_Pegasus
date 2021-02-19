e00020fcc9406e8bbe5568b35abc5207dcb60591
ZacJW <11651573+ZacJW@users.noreply.github.com>
2021-02-19 18:40:05 +0000

    Added wifi credentials from to hotspot

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,30 @@
 #include <string.h>
 #include <WiFi.h>
 #include <WiFiClient.h>
+#include <WebServer.h>
 #include <WiFiAP.h>
+#include <Wire.h>               // Only needed for Arduino 1.6.5 and earlier
+#include "SSD1306Wire.h"   
 // #include <BLEDevice.h>
 
-WiFiServer server;
+SSD1306Wire display(0x3c, SDA, SCL);
+WebServer web_server(80);
+extern const char wifi_form_html[] asm("_binary_src_wifi_form_html_start");
 
+struct WiFi_creds{
+    String SSID;
+    String PSK;
+};
+
+WiFi_creds get_wifi_creds();
 
 void setup() {
   Serial.begin(115200);
+  display.init();
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_24);
+  display.drawString(0, 0, "Hello world");
+  display.display();
   // put your setup code here, to run once:
   bool hasWiFi = EEPROM.read(0) == 1;
   String SSID = "";
@@ -31,26 +47,42 @@ void setup() {
   if (hasWiFi){
 
   }else{
-    unsigned char mac[6];
-    WiFi.macAddress(mac);
-    char id[7] = {0};
-    SSID = "Pegassas-Probe-";
-    snprintf(id, 7, "%02X%02X%02X", mac[3], mac[4], mac[5]);
-    SSID += id;
-    // BLEDevice::init("SSID");
-    // if (!BLEDevice::getInitialized()){
-    //   Serial.println("Bluetooth not running");
-    // }
-    uint32_t key_raw = esp_random();
-    char key[9] = {0};
-    snprintf(key, 9, "%08X", key_raw);
-    Serial.println(SSID);
-    Serial.println(key);
-    WiFi.softAP(SSID.c_str(), key);
-    server = WiFiServer(80);
+    get_wifi_creds();
   }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  web_server.handleClient();
 }
+
+
+WiFi_creds get_wifi_creds(){
+  String SSID;
+
+  unsigned char mac[6];
+  WiFi.macAddress(mac);
+  char id[7] = {0};
+  SSID = "Pegassas-Probe-";
+  snprintf(id, 7, "%02X%02X%02X", mac[3], mac[4], mac[5]);
+  SSID += id;
+  uint32_t key_raw = esp_random();
+  char key[9] = {0};
+  snprintf(key, 9, "%08X", key_raw);
+  Serial.println(SSID);
+  Serial.println(key);
+  WiFi.softAP(SSID.c_str(), "2DFD7BFB");
+  web_server.on("/", []() {
+    web_server.send(200, "text/html", wifi_form_html);
+  });
+  web_server.on("/form", [](){
+    Serial.println(web_server.args());
+    for (int i = 0; i < web_server.args(); ++i){
+      Serial.print(web_server.argName(i));
+      Serial.print(": ");
+      Serial.println(web_server.arg(i));
+    }
+    web_server.send(200, "text/html", "");
+  });
+  web_server.begin();
+}
+
