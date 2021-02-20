5af5ab9591074ffbb6770c939bd5b6227207286a
ZacJW <11651573+ZacJW@users.noreply.github.com>
2021-02-20 14:45:24 +0000

    OLED now displays the WiFi details when in hotspot

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,42 @@
 #include "SSD1306Wire.h"   
 // #include <BLEDevice.h>
 
-SSD1306Wire display(0x3c, SDA, SCL);
+SSD1306Wire display(0x3c, SDA, SCL, GEOMETRY_128_32);
 
 // HTML source code c-string built into program
 extern const char wifi_form_html[] asm("_binary_src_wifi_form_html_start");
+
+class Scroll_Text{
+  const char *str;
+  int16_t x, y, text_width, offset ;
+  uint8_t wait = 4;
+
+
+
+  public:
+    Scroll_Text(const char *str, int16_t x, int16_t y){
+      this->str = str;
+      this->x = x;
+      this->y = y;
+      text_width = display.getStringWidth(str, strlen(str));
+    }
+
+    void draw(){
+      if (wait){
+        --wait;
+      }else if (x + text_width - offset > display.width()){
+        ++offset;
+        if(x + text_width - offset == display.width()){
+          wait = 4;
+        }
+      }else{
+        offset = 0;
+        wait = 4;
+      }
+      display.drawString(x - offset, y, str);
+    }
+};
+
 
 struct WiFi_creds{
     String SSID;
@@ -25,7 +57,7 @@ void setup() {
   Serial.begin(115200);
   display.init();
   display.flipScreenVertically();
-  display.setFont(ArialMT_Plain_24);
+  display.setFont(ArialMT_Plain_10);
   display.drawString(0, 0, "Hello world");
   display.display();
   // put your setup code here, to run once:
@@ -74,16 +106,26 @@ WiFi_creds get_wifi_creds(){
   SSID = "Pegassas-Probe-";
   snprintf(id, 7, "%02X%02X%02X", mac[3], mac[4], mac[5]);
   SSID += id;
-  uint32_t key_raw = esp_random();
+  
+  
+  #ifdef DEV_PSK
+  // WiFi PSK of hotspot
+  char key[] = DEV_PSK;
+  #else
   char key[9] = {0};
+  uint32_t key_raw = esp_random();
   snprintf(key, 9, "%08X", key_raw);
+  #endif
   Serial.println(SSID);
   Serial.println(key);
-  #ifdef DEV_PSK
-  WiFi.softAP(SSID.c_str(), DEV_PSK);
-  #else
   WiFi.softAP(SSID.c_str(), key);
-  #endif
+
+  display.clear();
+  char buffer[32];
+  display.drawStringf(0, 0, buffer, "SSID: %s", SSID.c_str());
+  display.drawStringf(0, 10, buffer, "PSK: %s", key);
+  display.drawString(0, 20, "This is a very long string to test what happens");
+  display.display();
   web_server.on("/", [&web_server]() {
     web_server.send(200, "text/html", wifi_form_html);
   });
@@ -97,8 +139,25 @@ WiFi_creds get_wifi_creds(){
     web_server.send(200, "text/html", "");
   });
   web_server.begin();
+  unsigned long draw_time = millis();
+  const int16_t DRAW_INTERVAL = 200;
+  const int16_t SSID_OFFSET = display.getStringWidth("SSID: ", 6);
+  Scroll_Text ssid_scroll = Scroll_Text(SSID.c_str(), SSID_OFFSET, 0);
   while (new_SSID.length() == 0 || new_PSK.length() < 8){
     web_server.handleClient();
+    unsigned long current_time = millis();
+
+    if (current_time > draw_time + DRAW_INTERVAL){
+      draw_time += DRAW_INTERVAL;
+      display.clear();
+      ssid_scroll.draw();
+      display.setColor(BLACK);
+      display.fillRect(0, 0, SSID_OFFSET, 15);
+      display.setColor(WHITE);
+      display.drawString(0,0, "SSID:");
+      display.drawStringf(0, 10, buffer, "PSK: %s", key);
+      display.display();
+    }
   }
   WiFi_creds creds;
   creds.SSID = new_SSID;
