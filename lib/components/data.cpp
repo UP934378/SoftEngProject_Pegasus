d254331850a3614600fba7c3c445c5cdeb081b82
UP890760 <15788917+UP890760@users.noreply.github.com>
2021-03-05 19:24:47 +0000

    return some random data

--- lib/components/data.cpp
+++ lib/components/data.cpp
@@ -21,80 +21,106 @@
 // }
 
 char * dataString () {
-    char* dataAsString;
-    snprintf(dataAsString, 2000, 
+    char * cellData = (char *) calloc(250 * 3, 1);
+    int cellNums = 3;
+    for (int i = 0; i < cellNums; i++){
+        
+        char * cellDataTemp = (char *) malloc(250 * 3);
+        char * thirdpointer ;
+        if (i == cellNums-1){
+            snprintf(cellDataTemp, 250 * 3, "{\"id\":%d,\"voltage\":{\"voltage\":%ld,\"unit\":\"mV\"}}", i, random(3000,4000));
+        } else {
+            snprintf(cellDataTemp, 250 * 3, "{\"id\":%d,\"voltage\":{\"voltage\":%ld,\"unit\":\"mV\"}},", i, random(3000,4000));
+        }
+        
+        int cellDataLen = strlen(cellData);
+        int temCellData = strlen(cellDataTemp);
+        thirdpointer = (char *) malloc(cellDataLen + temCellData + 1);
+        strcpy(thirdpointer, cellData);
+        strcpy(thirdpointer+cellDataLen, cellDataTemp);
+        thirdpointer[cellDataLen + temCellData] = 0;
+        free(cellDataTemp);
+        free(cellData);
+        cellData = thirdpointer;
+
+        // cellData += cellDataTemp;
+        
+        //cellData += "{\"id\":" + i + ",\"voltage\":{\"voltage\":" + random(3000, 4000) + ",\"unit\":\"mV\"}},";
+    }
+    char* dataAsString = (char *) malloc(2500);
+    snprintf(dataAsString, 2500, 
         "{"
         " \"cell\": {"
             "\"data\": ["
+                "%s"
+                    // "\"id\": 0,"
+                    // "\"battery_id\": 0,"
+                    // "\"voltage\": {"
+                    //     "\"voltage\": 3500,"
+                    //     "\"unit\": \"mV\""
+                    // "},"
+                    // "\"ballance_current\": {"
+                    //     "\"current\": 1500,"
+                    //     "\"unit\": \"mA\""
+                    // "},"
+                    // "\"cell_temp\": {"
+                    //     "\"temp\": 35,"
+                    //     "\"unit\": \"C\""
+                    // "}"
+            "]"
+        "},"
+        "\"battery\": {"
+            "\"data\": {"
+                "\"battery_current\": {"
+                    "\"current\": 7000,"
+                    "\"unit\": \"mA\""
+                "},"
+                "\"state_charge\": {"
+                    "\"charge\": 200,"
+                    "\"unit\": \"Wh\""
+                "},"
+                "\"charge_perc\": {"
+                    "\"charge\": 79,"
+                    "\"unit\": \"%\""
+                "},"
+                "\"battery_temp\": {"
+                    "\"temp\": 40,"
+                    "\"unit\": \"c\""
+                "}"
+            "}"
+        "},"
+        "\"BMS_temp\": {"
+            "\"temp\": 32,"
+            "\"unit\": \"C\""
+        "},"
+        "\"inverter\": {"
+            "\"data\": ["
                 "{"
                     "\"id\": 0,"
-                    "\"voltage\": {"
-                        "\"voltage\": 3500,"
-                        "\"unit\": \"mV\""
+                    "\"inv_voltage\": {"
+                        "\"voltage\": 240,"
+                        "\"unit\": \"V\""
                     "},"
-                    "\"ballance_current\": {"
-                        "\"current\": 1500,"
-                        "\"unit\": \"mA\""
+                    "\"inv_power\": {"
+                        "\"power\": 349,"
+                        "\"unit\": \"W\""
                     "},"
-                    "\"cell_temp\": {"
-                        "\"temp\": 35,"
-                        "\"unit\": \"C\""
+                    "\"inv_frequency\": {"
+                        "\"frequency\": 50,"
+                        "\"unit\": \"Hz\""
                     "}"
                 "}"
-            "]"  
-        "},"  
-        "\"battery\": {"    
-            "\"data\": {"      
-                "\"battery_current\": {"        
-                    "\"current\": 7000,"        
-                    "\"unit\": \"mA\""      
-                "},"      
-                "\"state_charge\": {"        
-                    "\"charge\": 200,"        
-                    "\"unit\": \"Wh\""      
-                "},"      
-                "\"charge_perc\": {"        
-                    "\"charge\": 79,"        
-                    "\"unit\": \"%\""      
-                "},"      
-                "\"battery_temp\": {"        
-                    "\"temp\": 40,"       
-                    "\"unit\": \"c\""      
-                "}"    
-            "}"  
-        "},"  
-        "\"BMS_temp\": {"    
-            "\"temp\": 32,"    
-            "\"unit\": \"C\""  
-        "},"  
-        "\"inverter\": {"    
-            "\"data\": ["      
-                "{"        
-                    "\"id\": 0,"        
-                    "\"inv_voltage\": {"          
-                        "\"voltage\": 240,"          
-                        "\"unit\": \"V\""        
-                    "},"        
-                    "\"inv_power\": {"          
-                        "\"power\": 349,"          
-                        "\"unit\": \"W\""        
-                    "},"        
-                    "\"inv_frequency\": {"          
-                        "\"frequency\": 50,"          
-                        "\"unit\": \"Hz\""        
-                    "}"      
-                "}"    
-            "]"  
-        "},"  
-        "\"grid_power\": {"    
-            "\"power\": 3000,"    
-            "\"unit\": \"W\""  
-        "},"  
-        "\"house_power\": {"    
-            "\"power\": 3000,"    
-            "\"unit\": \"W\""  
+            "]"
+        "},"
+        "\"grid_power\": {"
+            "\"power\": 3000,"
+            "\"unit\": \"W\""
+        "},"
+        "\"house_power\": {"
+            "\"power\": 3000,"
+            "\"unit\": \"W\""
         "}"
         "}"
-    );
+    , cellData);
     return dataAsString;
 }
