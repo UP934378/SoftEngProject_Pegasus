2bfc2f994374a1df3b33951e92fb504706ed1b6e
ZacJW <11651573+ZacJW@users.noreply.github.com>
2021-03-09 11:23:47 +0000

    Updated parser and JSON schema
    Co-authored-by: UP934378 <UP934378@myport.ac.uk>

--- src/pegassas_aggregator/parser.rs
+++ src/pegassas_aggregator/parser.rs
@@ -97,23 +97,17 @@ pub struct BatteryData {
     state_charge : Charge,
     charge_perc : Charge,
     balance_current : Current,
-    battery_temp : Temperature
+    battery_temp : Option<Temperature>
 }
 
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SolarData {
     id: String,
-    inv_voltage : Voltage,
-    inv_power : Power,
-    inv_frequency : Frequency
+    sol_inv_voltage : Voltage,
+    sol_inv_power : Power,
+    sol_inv_frequency : Frequency
 }
-
-
-
-
-
-
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Cell {
@@ -150,6 +144,7 @@ pub struct Data {
 
 pub fn parse_data(data: Data) -> influx_db_client::Points {
     let mut points_vector = std::vec::Vec::new();
+    // Cell Data - pointers to InfluxDB - Uses data_schema.json
     if let Some(cell) = data.cell {
         for cell_data in cell.data {
             let point = influx_db_client::Point::new("cell")
@@ -159,6 +154,46 @@ pub fn parse_data(data: Data) -> influx_db_client::Points {
                         .add_field("balance_current", i64::from(cell_data.balance_current.current));
             points_vector.push(point);
         }
+    }
+    // Battery Data - pointers to InfluxDB - Uses data_schema.json
+    if let Some(battery) = data.battery {
+        let point = influx_db_client::Point::new("battery")
+            .add_tag("probe_id", data.probe_id)
+            .add_field("battery_current", i64::from(battery.data.balance_current.current))
+            .add_field("state_charge", i64::from(battery.data.state_charge.charge))
+            .add_field("charge_perc", i64::from(battery.data.charge_perc.charge));
+        let point = match battery.data.battery_temp { //Temp here = temperature of battery
+            Some(temp) => point.add_field("battery_temp", i64::from(temp.temp)),
+            None => point
+        };
+        points_vector.push(point);
+    }
+
+    // Solar Data - pointers to InfluxDB - Uses data_schema.json
+    if let Some(solar) = data.solar {
+        for solar_data in solar.data {
+            let point = influx_db_client::Point::new("inverter")
+                        .add_tag("solar_data", solar_data.id)
+                        .add_tag("probe_id", data.probe_id)
+                        .add_field("sol_inv_voltage", i64::from(solar_data.sol_inv_voltage.voltage))
+                        .add_field("sol_inv_power", i64::from(solar_data.sol_inv_power.power))
+                        .add_field("sol_inv_frequency", i64::from(solar_data.sol_inv_frequency.frequency));
+            points_vector.push(point);
+        }
+    }
+
+    // Grid Power Data - pointers to InfluxDB - Uses data_schema.json
+     if let Some(grid_power) = data.grid_power {
+        let point = influx_db_client::Point::new("grid_power")
+            .add_tag("grid_power", i64::from(grid_power.power));
+        points_vector.push(point);
+    }
+
+    // House Power Data - pointers to InfluxDB - Uses data_schema.json
+    if let Some(house_power) = data.house_power {
+        let point = influx_db_client::Point::new("house_power")
+            .add_tag("house_power", i64::from(house_power.power));
+        points_vector.push(point);
     }
     // TODO: add point creation for:
     // - data.battery
