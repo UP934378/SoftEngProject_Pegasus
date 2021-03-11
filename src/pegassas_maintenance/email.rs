74228414352c21c6af9b834138ba5e2e039db150
UP934378 <71968053+UP934378@users.noreply.github.com>
2021-03-11 11:58:26 +0000

    update to email.rs added functionality

--- src/pegassas_maintenance/email.rs
+++ src/pegassas_maintenance/email.rs
@@ -2,29 +2,46 @@ use lettre::transport::smtp::authentication::Credentials;
 use lettre::{Message, SmtpTransport, Transport};
 
 //local Email server needs to be installed
+pub struct Email {
+    from: String, 
+    reply_to: String, 
+    recipent: String, 
+    subject: String, 
+    body: String
+}
 
 let sender : String
-match sender == String {
-    
-}
-let email = Message::builder()
-    .from("NoBody <nobody@domain.tld>".parse().unwrap())
-    .reply_to("Yuin <yuin@domain.tld>".parse().unwrap())
-    .to("Hei <hei@domain.tld>".parse().unwrap())
-    .subject("Happy new year")
-    .body("Be happy!")
-    .unwrap();
+impl Email {
+    pub fn new(from: String, reply_to: String, recipent: String, subject: String, body: String) -> Email {
+        from, 
+        reply_to, 
+        recipent, 
+        subject, 
+        body
+    }
 
-let creds = Credentials::new("smtp_username".to_string(), "smtp_password".to_string());
+    pub fun buildEmail{
+        let email = Message::builder()
+            .from(from.parse().unwrap())
+            .reply_to(reply_to.parse().unwrap())
+            .to(recipent.parse().unwrap())
+            .subject(subject)
+            .body(body)
+            .unwrap();
+    }
+    //Cred held locally within maintenance session.
+    let creds = Credentials::new("smtp_username".to_string(), "smtp_password".to_string());
 
-// Open a remote connection to gmail
-let mailer = SmtpTransport::relay("smtp.gmail.com")
-    .unwrap()
-    .credentials(creds)
-    .build();
-
-// Send the email
-match mailer.send(&email) {
-    Ok(_) => println!("Email sent successfully!"),
-    Err(e) => panic!("Could not send email: {:?}", e),
+    pub fn sendEmail(&Email, creds) {
+        // Open a remote connection to email server
+        let mailer = SmtpTransport::relay("") //local server Address
+            .unwrap()
+            .credentials(creds)
+            .build();
+        match mailer.send(&email) {
+            Ok(_) => {println!("Email sent successfully!"); 
+            lettre::file::FileTransport new ("")},// local area to write email logs
+            Err(e) => panic!("Could not send email: {:?}", e),
+        }
+    }
 }
