a6ce76b9ea9eecbea2e424c7d5ed90bbcd690aa1
ZacJW <11651573+ZacJW@users.noreply.github.com>
2021-03-14 16:57:04 +0000

    Implemented worker

--- src/pegassas_aggregator/worker.rs
+++ src/pegassas_aggregator/worker.rs
@@ -1,55 +1,135 @@
 use std::time::{Duration, Instant};
 use std::string::String;
-use serde_json::{Deserializer, Value};
+use serde_json::Deserializer;
+use std::ops::Deref;
+use std::sync::{Arc, Mutex};
+use tokio::runtime::Runtime;
 use crate::pegassas_aggregator::parser::{Data, parse_data};
 
-pub struct ProbeUrl{
-    ip_address: std::net::Ipv4Addr,
-    port: u16,
-    location: String,
-}
-
 pub struct ProbeWorker {
-    url : ProbeUrl,
+    pub url : std::sync::Mutex<String>,
     usn: String,
-    ttl: Instant,
-    request_interval: Duration
+    pub ttl: Mutex<Instant>,
+    request_interval: Duration,
+    pub should_work: Mutex<bool>,
+    rt: Arc<Runtime>
 }
 
 impl ProbeWorker {
-    pub fn new(url: ProbeUrl, usn: String) -> ProbeWorker {
-        let ttl = Instant::now() + Duration::from_secs(10);
-        let request_interval = Duration::from_secs(30);
+    pub fn new(usn: String, rt: Arc<Runtime>) -> ProbeWorker {
         ProbeWorker {
-            url,
+            url: std::sync::Mutex::new("".to_string()),
             usn,
-            ttl,
-            request_interval
+            ttl: Mutex::new(Instant::now() + Duration::from_secs(60)),
+            request_interval: Duration::from_secs(30),
+            should_work: Mutex::new(true),
+            rt
+        }
+    }
+
+    fn check_should_work(&self) -> bool {
+        match self.should_work.lock(){
+            Ok(b) => *b,
+            Err(_) => false
+        }
+    }
+
+    fn check_ttl(&self) -> bool {
+        match self.ttl.lock(){
+            Ok(ttl) => Instant::now() < *ttl,
+            Err(_) => false
+        }
+    }
+
+    fn get_data_url(&self, response: ssdp_client::SearchResponse) -> Option<String> {
+        let http_client = reqwest::Client::new();
+        // Request schema from probe
+        let schema_tree = match reqwest::Url::parse(response.location()) {
+            Ok(schema_url) => {
+                match self.rt.block_on(http_client.get(schema_url).send()) {
+                    Ok(a) => {
+                        match self.rt.block_on(a.text()) {
+                            Ok(schema_text) => {
+                                match xmltree::Element::parse(schema_text.as_bytes()){
+                                    Ok(xml) => Some(xml),
+                                    // TODO: Add error handling
+                                    Err(e) => None
+                                }
+                            },
+                            // TODO: Add error handling
+                            Err(e) => None
+                        }
+                    },
+                    // TODO: Add error handling
+                    Err(e) => None
+                }
+            },
+            // TODO: Add error handling
+            Err(e) => None
+        };
+
+        match schema_tree {
+            Some(ref st) => {
+                match st.get_child("device") {
+                    Some(device) => {
+                        match device.get_child("presentationURL") {
+                            Some(presentation_url) => match presentation_url.get_text() {
+                                Some(url) => Some(url.into_owned()),
+                                None => None
+                            },
+                            None => None
+                        }
+                    },
+                    None => None
+                }
+            },
+            None => None
+        }
+    }
+
+    pub fn update(&self, response: ssdp_client::SearchResponse) {
+        match self.url.lock() {
+            Ok(mut url) => *url = match self.get_data_url(response){
+                Some(s) => s,
+                None => return
+            },
+            Err(e) => ()
+        }
+        match self.ttl.lock() {
+            Ok(mut ttl) => *ttl = Instant::now() + Duration::from_secs(60),
+            Err(e) => ()
         }
     }
     
 
-    pub async fn run(&self){
-        loop{
+    pub fn run(&self) {
+        while self.check_should_work() && self.check_ttl() {
+            let loop_start_instant = Instant::now();
+            let url;
+            match self.url.lock() {
+                Ok(a) => {
+                    url = a.deref().clone();
+                },
+                Err(e) => {
+                    println!("Couldn't acquire url mutex: {:?}", e);
+                    continue
+                }
+            };
+            println!("{:?}", url);
             let http_client = reqwest::Client::new();
-            let mut request_url_string = String::from("http://");
-            request_url_string += format!("{}", self.url.ip_address).as_str();
-            request_url_string += ":";
-            request_url_string += format!("{}", self.url.port).as_str();
-            request_url_string += self.url.location.as_str();
-            let request_url = reqwest::Url::parse(request_url_string.as_str());
+            let request_url = reqwest::Url::parse(url.as_str());
             let request_url = match request_url {
                 Ok(r) => Some(r),
                 Err(_) => {
-                    println!("error parsing url: {}", request_url_string);
+                    println!("error parsing url: {}", url);
                     None
                 }
             };
             let response = match request_url {
-                Some(r) => Some(http_client.get(r).send().await),
+                Some(r) => Some(self.rt.block_on(http_client.get(r).send())),
                 None => None
             };
-            // TODO: add logging for posible error that includes details of the error
+            // TODO: add logging for possible error that includes details of the error
             let response = match response {
                 Some(Ok(r)) => Some(r),
                 _ => {
@@ -59,7 +139,7 @@ impl ProbeWorker {
             };
 
             let json_string = match response {
-                Some(r) => match r.text().await {
+                Some(r) => match self.rt.block_on(r.text()) {
                     Ok(r) => Some(r),
                     Err(e) => None
                 }
@@ -79,7 +159,7 @@ impl ProbeWorker {
                             Ok(d) => {
                                 let influx_client = influx_db_client::Client::new(reqwest::Url::parse("http://localhost:8086").unwrap(), "pegassas");
                                 let points = parse_data(d);
-                                influx_client.write_points(points, Some(influx_db_client::Precision::Seconds), None).await;
+                                self.rt.block_on(influx_client.write_points(points, Some(influx_db_client::Precision::Seconds), None));
                             },
                             Err(_) => ()
                         }
@@ -87,7 +167,13 @@ impl ProbeWorker {
                 },
                 None => ()
             }
-
+            // Wait for discovery interval to expire
+            match self.request_interval.checked_sub(loop_start_instant.elapsed()){
+                Some(wait_duration) => {
+                    std::thread::sleep(wait_duration);
+                },
+                None => ()
+            };
         }
     }
 
