0eab5d82f61f2add929907365ebcba8bcfafa0df
ZacJW <11651573+ZacJW@users.noreply.github.com>
2021-03-20 11:26:34 +0000

    Regenerated aggregator-docs

---
+++ docs/code_docs/doc/pegassas_aggregator/pegassas_aggregator/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"mod":[["discovery",""],["parser",""],["worker",""]]});
---
+++ docs/code_docs/doc/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["pegassas_aggregator"] = {"name":"","dirs":[{"name":"pegassas_aggregator","files":["discovery.rs","mod.rs","parser.rs","worker.rs"]}],"files":["main.rs"]};
+createSourceSidebar();
