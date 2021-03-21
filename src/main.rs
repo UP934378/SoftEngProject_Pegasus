3f561d3bf27b4f62b41f3fecc302193f2d9e447b
ZacJW <11651573+ZacJW@users.noreply.github.com>
2021-03-21 20:49:57 +0000

    Began adding logging and probe config support

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,61 @@ use std::collections::hash_map::HashMap;
 use tokio::runtime::Runtime;
 use std::sync::{Arc, Mutex};
 use std::time::{Duration, Instant};
+use log::{debug, error, log_enabled, info, Level};
 
-fn main() {
-    let rt = Arc::new(match Runtime::new() {
-        Ok(r) => r,
+
+
+struct ProbeConfig{
+    frequency: i32,
+    should_collect: bool
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>>{
+    env_logger::init();
+
+
+    let rt = Arc::new(match Runtime::new(){
+        Ok(runtime) => runtime,
         Err(e) => {
-            println!("Could not create tokio runtime: {:?}", e);
-            return;
+            error!("Failed to create tokio runtime: {:?}", e);
+            return Err(Box::new(e));
         }
     });
 
     let mut workers : HashMap<String, (Arc<ProbeWorker>, std::thread::JoinHandle<()>)> = HashMap::new();
-    // let mut workers  = std::vec::Vec::new();
     let http_client = reqwest::Client::new();
-    println!("Aggregator started");
+    let host = "localhost";
+    let user = "aggregator";
+    let dbname = "pegassas";
+    let mut postgres_client = match postgres::Client::connect(format!("host={} user={} dbname={}", host, user, dbname), postgres::NoTls){
+        Ok(c) => {
+            info!("Connection to postgres database successful. host: {}, user: {}, dbname: {}", host, user, dbname);
+            c
+        },
+        Err(e) => {
+            error!("Failed to connect to postgres database: {} at host: {} with user: {}", dbname, host, user);
+            return Err(Box::new(e));
+        }
+    };
+    info!("Aggregator started successfully");
+    let mut check_instant = Instant::now();
+    let mut probe_configs : HashMap<String, ProbeConfig> = HashMap::new();
     loop {
+        if Instant::now() >= check_instant {
+            let probe_configs_vec = match postgres_client.query("SELECT probe_id, request_interval FROM probe_config;", &[]) {
+                Ok(rows) => {
+                    check_instant = Instant::now();
+                    rows
+                },
+                Err(e) => {
+                    error!("Failed to get probe configurations from database: {:?}", e);
+                    Vec::new()
+                }
+            };
+            for config in probe_configs_vec {
+                // TODO: parse rows into probe_configs hashmap
+            }
+        }
         let start_discovery_instant = Instant::now();
         let mut responses = std::vec::Vec::new();
         match rt.block_on(discovery::discover_probes()) {
