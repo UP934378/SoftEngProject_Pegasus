f65d69f403474b145f30aaedfa02db8678fe8caa
ZacJW <11651573+ZacJW@users.noreply.github.com>
2021-03-30 20:35:36 +0100

    WIP adding discovery updates probe_config

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod discovery;
 mod worker;
 mod parser;
+mod request;
 use worker::ProbeWorker;
 
 use std::collections::hash_map::HashMap;
@@ -8,18 +9,19 @@ use tokio::runtime::Runtime;
 use std::sync::{Arc, Mutex};
 use std::time::{Duration, Instant};
 use log::{debug, error, log_enabled, info, Level};
+use eui48::MacAddress;
 
 
 
 struct ProbeConfig{
     frequency: i32,
-    should_collect: bool
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
+    // Setup logging
     env_logger::init();
 
-
+    // Setup tokio runtime
     let rt = Arc::new(match Runtime::new(){
         Ok(runtime) => runtime,
         Err(e) => {
@@ -27,9 +29,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
             return Err(Box::new(e));
         }
     });
-
+    
     let mut workers : HashMap<String, (Arc<ProbeWorker>, std::thread::JoinHandle<()>)> = HashMap::new();
     let http_client = reqwest::Client::new();
+
+    // Connect to postgreSQL database
     let host = "localhost";
     let user = "aggregator";
     let dbname = "pegassas";
@@ -44,9 +48,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
         }
     };
     info!("Aggregator started successfully");
+
     let mut check_instant = Instant::now();
-    let mut probe_configs : HashMap<String, ProbeConfig> = HashMap::new();
+    let mut probe_configs : HashMap<MacAddress, ProbeConfig> = HashMap::new();
+
+    // Main loop
     loop {
+        // Refresh probe_configs from database
         if Instant::now() >= check_instant {
             let probe_configs_vec = match postgres_client.query("SELECT probe_id, request_interval FROM probe_config;", &[]) {
                 Ok(rows) => {
@@ -59,16 +67,57 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
                 }
             };
             for config in probe_configs_vec {
-                // TODO: parse rows into probe_configs hashmap
+                let probe_id : MacAddress = match config.try_get("probe_id") {
+                    Ok(id) => id,
+                    Err(e) => {
+                        error!("Failed to get probe_id from configuration response: {:?}", e);
+                        continue;
+                    }
+                };
+
+                let frequency : i32 = match config.try_get("request_interval") {
+                    Ok(f) => f,
+                    Err(e) => {
+                        error!("Failed to get request_interval from configuration response: {:?}", e);
+                        continue;
+                    }
+                };
+                probe_configs.insert(probe_id, ProbeConfig{frequency});
             }
         }
+
+        // Discover data probes
         let start_discovery_instant = Instant::now();
         let mut responses = std::vec::Vec::new();
         match rt.block_on(discovery::discover_probes()) {
             Ok(mut v) => responses.append(&mut v),
-            Err(e) => println!("{:?}", e)
-        }
+            Err(e) => error!("probe discovery failed with error: {:?}", e)
+        };
         for response in responses {
+            match request::get_data_url(&rt, &response){
+                Some(url) => {
+                    match request::make_request(&url, &rt){
+                        Ok(stream) => {
+                            let mut stream_iter = stream.into_iter::<parser::Data>();
+                            match stream_iter.next(){
+                                Some(Ok(data)) => {
+                                    
+                                },
+                                Some(Err(e)) => {
+
+                                },
+                                None => {
+
+                                }
+                            }
+                        },
+                        Err(e) => {
+
+                        }
+                    };
+                },
+                None => error!("")
+            }
             let usn = response.usn().to_string();
             
             match workers.get(&usn) {
@@ -89,6 +138,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
                 }
             };
         }
+        
+        // Wait until next loop
         match Duration::from_secs(20).checked_sub(start_discovery_instant.elapsed()) {
             Some(wait_duration) => std::thread::sleep(wait_duration),
             None => ()
